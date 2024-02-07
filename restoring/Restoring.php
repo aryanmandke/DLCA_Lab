e6430b28e6b8c87ac89d542d8cf461d2269c7b21
Alpharius397 <155239372+Alpharius397@users.noreply.github.com>
2024-02-07 10:23:37 +0530

    Add files via upload

---
+++ restoring/Restoring.php
@@ -0,0 +1,451 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+ body {
+ font-family: Arial, sans-serif;
+ margin: 0px auto;
+ padding: 10px;
+ background: url("../images/sim.jpg") no-repeat center center fixed;
+ background-size: cover;
+ justify-content: center;
+ display: flex;
+ align-items: center;
+ }
+ 
+ #container {
+width: 1000px;
+ margin: 10px auto;
+ padding: 50px;
+ background-color: rgba(255, 255, 255, 0.9);
+ box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+ border-radius: 5px;
+ text-align: center;
+ }
+ 
+ h1 {
+ color: #971426;
+ }
+ 
+ label {
+ font-size: 16px;
+ color: #333;
+ margin-right: 10px;
+ }
+ 
+ input[type="number"] {
+ width: 40px;
+ padding: 6px;
+ font-size: 16px;
+ border: 1px solid #ccc;
+ border-radius: 4px;
+ }
+ 
+ #calculate,
+ #reset,
+ #check-next,
+ #check-back {
+ font-size: 14px; /* Adjusted font size */
+ padding: 10px 14px; /* Adjusted padding */
+ background-color: #0074d9;
+ color: #fff;
+ border: none;
+ border-radius: 4px;
+ cursor: pointer;
+ margin-top: 10px;
+ }
+ 
+ table {
+ width: 100%;
+ border-collapse: collapse;
+ margin-top: 20px;
+ }
+ 
+ th,
+ td {
+ border: 1px solid black;
+ padding: 8px;
+ text-align: center;
+ }
+ 
+ th {
+ background-color: #333;
+ color: #fff;
+ border: 1px solid #ffffff;
+ }
+ 
+ tr:nth-child(even) {
+ background-color: #f2f2f2;
+ }
+ 
+ #comment {
+ padding: 8px;
+ text-align: center;
+ border: 1px solid black;
+ width: 200px;
+ height: max-content;
+ margin: auto;
+ }
+
+ .tab{
+    width: 100%;
+ }
+ 
+ #result {
+ margin-top: 20px;
+ font-size: 20px;
+ margin-bottom: 20px;
+ }
+ 
+ #con {
+ padding: 8px;
+ text-align: center;
+ border: 0px solid #ffffff;
+ width: 100px;
+ margin: auto;
+ }
+
+
+    </style>
+</head>
+<body>
+    <div id="container">
+        <h1>Restoring Division Algorithm Simulation</h1>
+        <h2>Enter the numbers to be divided in decimal format.</h2>
+
+        <label for="first">Dividend: </label>
+        <input type="number" id="first"  min="1">
+        <label for="second">Divisor: </label>
+        <input type="number" id="second" min="1">
+        <button id="calculate">Calculate</button>
+
+        <table id="algoTable">
+            <thead>
+                <tr>
+                        <th>Count</th>
+                        <th>M</th>
+                        <th>AC</th>
+                        <th>Q</th>
+                        <th>Action</th>
+                    </tr>
+            </thead>
+            <tbody id="confirm"></tbody>
+            <tbody id="algoBody">
+                <td colspan="5" id="comment"></td>
+                <td id="con">
+                <div class="control">   
+                <button id="check-next" disabled="true" style="background: grey;">Next Step</button><button id="check-back" disabled="true" style="background: grey;">Previous Step</button></div></td>
+            </tbody>
+        </table>
+        <div id = "result"></div>
+    </div>
+        
+    
+   
+    <div><script>
+        function po(n) {
+    let i = 1;
+    for (let k = 0; k < n; k++) {
+        i = i * 2;
+    }
+    return i;
+}
+
+
+function odd(a) {
+    if (a % 2 === 0) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+function buttonEnabled(next){
+        next.disabled = false;
+        next.style.background = "#00904a";
+        next.style.cursor = 'pointer';
+}
+
+function buttondisnabled(next){
+        next.disabled = true;
+        next.style.background = "grey";
+        next.style.cursor = 'default';
+}
+
+
+function bina(N, Cnt) {
+    let i, j = 1,loop = 1;
+    let ans="";
+    i=N%2;
+        N=Math.floor(N/2);
+            ans="_"+ans;
+    while(N!=0){   
+        i=N%2;
+        N=Math.floor(N/2);
+        ans=String(i)+ans;
+        j+=1;
+    }
+    while(j<Cnt){
+    ans="0"+ans;
+    j+=1;
+    }
+    return ans;
+}
+
+function binary(N, Cnt) {
+    let i, j = 1;
+    let ans="";
+    while(N!=0){
+        i=N%2;
+        N=Math.floor(N/2);
+        ans=String(i)+ans;
+        j+=1;
+    }
+    while(j<=Cnt){
+    ans="0"+ans;
+    j+=1;
+    }
+    return ans;
+}
+
+function restoring(M,Q){
+   var deck = 0;
+
+       let A = 0;;
+       let flag = 0;
+       let n = 0;
+       let b, c, d, e;
+       let nQ, nM;
+       let loop, count = 1, Cnt;
+       let ac,q,r,m;
+  
+
+       while (true) {
+           b = po(n);
+           if (Q >= b) {
+               n += 1;
+               count += 1;
+           } else {
+               break;
+           }
+       }
+  
+       b = po(count - 1);
+       c = po(count);
+       d = c - 1;
+       e = b / 2;
+       f = po(count + 1) - 1;
+ 
+       Cnt = count;
+  
+       nM = (d - M) + 1;
+  
+   let Morg = M, Qorg = Q;
+       while (count != 0) {
+          
+           if(count == Cnt){
+               act = "Initialization";
+               ac = binary(A, Cnt);
+               q = binary(Q, Cnt);
+               m = binary(M , Cnt);       
+               result[deck]={count,m,ac,q,act};
+               deck +=1;
+           }
+
+           A=A*2;
+           Q=Q*2;
+
+           if(A > d){
+               A = A % c
+           }
+           if(Q > d){
+               A = A + 1;
+               Q = Q % c;
+           }
+               act = "Left Arithmetic Shift";
+               ac = binary(A, Cnt);
+               q = bina(Q, Cnt);
+               m = binary(M , Cnt);
+               result[deck]={count,m,ac,q,act};
+               deck +=1;
+
+           A = A + nM
+
+         if(A > d){
+               A = A % c
+           }
+
+           act = "A = A - M";
+           ac = binary(A, Cnt);
+           q = bina(Q, Cnt);
+           m = binary(M , Cnt);
+
+           result[deck]={count,m,ac,q,act};
+           deck +=1;
+       if(A >= b){
+           A = A - nM;
+           act = "Q[0] = 0, A = A + M";
+
+           ac = binary(A, Cnt);
+          q = binary(Q, Cnt);
+       m = binary(M , Cnt);
+
+       result[deck]={count,m,ac,q,act};
+       deck +=1;
+       }
+       else{
+           Q = Q + 1;
+           act = "Q[0] = 1";
+       ac = binary(A, Cnt);
+       q = binary(Q, Cnt);
+       m = binary(M , Cnt);
+
+       result[deck]={count,m,ac,q,act};
+       deck +=1;
+       }
+  
+       count-=1;
+       }
+
+       if(A >= b){
+                A = A + M;
+                if(A > d){
+            A = A % c
+        }
+        ac = binary(A, Cnt);
+            act = "A = A + M";
+            q = binary(Q, Cnt);
+    m = binary(M , Cnt);
+
+       result[deck]={count,m,ac,q,act};
+            }
+
+
+               console.log(result);
+}
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function find_state(cnt){
+    let a = result[cnt].act;
+    let b = result[cnt].count;
+
+    if(b==0){
+        p.innerHTML = `As <b>A</b> is less than zero (as sign bit of A is 1), we add <b>M</b> (Divisor) into <b>A</b>.Thus A → A + M.`;
+    }
+
+    switch(a){
+        case "A = A - M":p.innerHTML = `${opt[2]}`; break;
+        case "Q[0] = 1":p.innerHTML = `${opt[4]}`; break;
+        case "Initialization":p.innerHTML = `${opt[0]}`; break;
+        case "Left Arithmetic Shift":p.innerHTML = `${opt[3]}`; break;
+        case "Q[0] = 0, A = A + M":p.innerHTML = `${opt[1]}`; break;
+    }
+}
+
+var result = [];
+var opt = ["Initialization","As <b>A</b> is less than zero (as sign bit of A is 1),we assign Q<sub>0</sub> as 0. Additionally we add <b>M</b> (Divisor) into <b>A</b>.Thus, A → A + M. Furthermore we decreement the <b>Count</b> by 1.","Subtracting <b>M</b> (Divisor) from <b>A</b>.Thus, A → A - M.","We perform Left Arithmetic Shift on <b>A</b> and <b>Q</b>.","As <b>A</b> is greater than zero (as sign bit of A is 0),we assign Q<sub>0</sub> as 1. Additionally we decreement the <b>Count</b> by 1."];
+var cnt = 0;
+const button = document.getElementById('calculate');
+const back = document.getElementById('check-back');
+const next = document.getElementById('check-next');
+const p = document.getElementById('comment');
+const ans = document.getElementById('confirm');
+const re = document.getElementById('result');
+var Q,M;
+
+button.addEventListener('click', async () =>{
+    result =[];
+    buttondisnabled(next);buttondisnabled(back);
+    next.style.display = '';
+    back.style.display = ''; 
+    p.style.display = '';
+    con.style.display = '';
+    ans.innerHTML = ``;
+    re.innerHTML = ``;
+    cnt = 0;
+
+
+    M = parseInt(document.getElementById('first').value);
+    Q = parseInt(document.getElementById('second').value);
+
+    if (!(isNaN(M) || isNaN(Q))){
+        restoring(Q,M);
+        var count = result[0].count;
+        ans.innerHTML = '';
+        p.innerHTML = '';
+
+        const giv = document.createElement('tr');
+        giv.innerHTML = `<td>${result[cnt].count}</td>
+                    <td>${result[cnt].m}</td>
+                    <td>${result[cnt].ac}</td>
+                    <td>${result[cnt].q}</td>
+                    <td>${result[cnt].act}</td>`;
+        ans.appendChild(giv);
+        find_state(cnt);
+        cnt += 1;
+
+        button.style.background = "grey";
+        button.disabled = true;
+
+        await sleep(1000);
+
+        buttonEnabled(next);
+        button.style.background = "#0074d9";
+        button.disabled = false;
+        console.log(result);
+    }
+    else{
+        console.log("Null values detected!");
+    }
+    });
+
+    next.addEventListener('click', async () => {
+        cnt += 1;    
+        const giv = document.createElement('tr');
+        giv.innerHTML = `<td>${result[cnt].count}</td>
+                    <td>${result[cnt].m}</td>
+                    <td>${result[cnt].ac}</td>
+                    <td>${result[cnt].q}</td>
+                    <td>${result[cnt].act}</td>`;
+        ans.appendChild(giv);
+        find_state(cnt);
+        
+
+    buttonEnabled(back)
+
+    if(cnt>=result.length-1){
+        buttondisnabled(next);
+        re.innerHTML = `<h1>Final Answer</h1><p> Q = (${M})<sub>10</sub> = (${result[0].q})<sub>2</sub><br>
+                M = (${Q})<sub>10</sub> = (${result[0].m})<sub>2</sub><br>
+                Quotient = (${Math.floor(M/Q)})<sub>10</sub> = (${result[result.length-1].q})<sub>2</sub><br>
+                Remainder = (${M%Q})<sub>10</sub> = (${result[result.length-1].ac})<sub>2</sub></p>`;
+    }
+
+});
+
+back.addEventListener('click', async () => {
+    
+    if(cnt>=result.length-1){
+        re.innerHTML = ``;
+    }
+    
+    cnt-=1;
+    ans.removeChild(ans.lastChild);
+    find_state(cnt);
+
+    if(cnt<=1){
+        buttondisnabled(back);
+;
+    }
+    buttonEnabled(next)
+
+});
+       
+    </script>
+    </div>
+</body>
+
+</html>
---
+++ restoring/restfeed.php
@@ -0,0 +1,257 @@
+<?php  
+session_start();
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feedback Form</title>
+    <style>
+ body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0px;
+        }
+
+        form {
+            max-width: 500px;
+            margin: 0 auto;
+            background: #fff;
+            padding: 20px;
+            border-radius: 6px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            transition: 0.6s ease-out;
+            
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+        }
+
+        #username, #email {
+            width: 50%;
+            padding: 8px;
+            margin-bottom: 15px;
+            height: 10px;
+            text-align:left;
+            display: inline-block;
+            margin-top: 15px;
+        }
+        
+        textarea{
+            width: 97%;
+            padding: 8px;
+            font-family: Arial, sans-serif;
+            margin: 8px 0px 15px;
+            border: 3px solid #ccc;
+            border-radius: 5px;
+            height: 20px;
+            text-align:left;
+        }
+
+        header a{
+            text-decoration: none;
+            font-weight: bold;
+            color: #ffffff;
+        }
+
+        .sub{
+            margin: auto;
+            display: flex;
+            width: 80%;
+            padding-top: 10px;
+        }
+
+        header {
+            display: flex;
+            background-color: #971426;
+            padding: 20px;
+            color: #ffffff;
+            width: 100%;
+            box-sizing: border-box;
+        }
+
+        header h4 {
+            margin: auto;
+            text-align: center;
+        }
+
+        input[type="submit"] {
+            background-color: #971426;
+            color: #ffffff;
+            padding: 10px 20px;
+            width: 100%;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            margin-top: 20px;
+            margin: auto;
+        }
+
+        #error{
+            margin: auto;
+            text-align: center;
+            width: 100%;
+            padding: 8px;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+
+        .rating {
+            margin: 5px;
+            text-align:justify;
+            display: flex;
+            border: 3px solid #ccc;
+            border-radius: 5px;
+            padding: 10px;
+            margin: 0;
+            margin-bottom: 15px;
+        }
+
+        .star{
+            margin: 5px;
+            text-align: center;
+        }
+
+        .rating {
+            display: inline-block;
+        }
+
+        .star input[type="radio"] {
+            display: none;
+        }
+
+        .star input[type="radio"] {
+            display: none;
+        }
+
+        .star label {
+            font-size: 35px;
+            color: #ccc;
+            cursor: pointer;
+            display: inline-block; 
+            text-align: center;
+            margin: 5px;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        .star label:before {
+            content: '\2605'; 
+            display: block;
+        }
+
+        .star input[type="radio"]:checked + label {
+            color: gold;
+        }
+    </style>
+</head>
+<body>
+<header>
+        <a href="RestoringQuiz.php" class="back-button">Back</a>
+        <h4>Restoring Division Algorithm</h4>
+        <a style="-ms-user-select: none;user-select: none;color: #971426;" class="white">Back</a>
+    </header><main><br><br><br>
+
+    <form action="feed-l.php" method="post" id="feedbackForm" onsubmit="return validate();">
+        <label for="name">Username:<p id= "username"> <?php echo $_SESSION['user']?></p></label>
+        
+
+        <label for="name">Email:<p id = "email"><?php echo $_SESSION['email'] ?></p></label>
+        
+        <label for="feed">Feedback</label>
+        <div class="rating">
+            <ol>
+                <li>
+                    <p>How would you rate your overall experience with this virtual lab experiment?</p>
+                    <div class="star">
+                        <input type="radio" name="q1" value="5" id="s1"> <label for="s1"></label>
+                        <input type="radio" name="q1" value="4" id="s2"> <label for="s2"></label>
+                        <input type="radio" name="q1" value="3" id="s3"> <label for="s3"></label>
+                        <input type="radio" name="q1" value="2" id="s4"> <label for="s4"></label>
+                        <input type="radio" name="q1" value="1" id="s5"> <label for="s5"> </label>
+                    </div>
+                </li><br>
+                <li>
+                    <p>Were you able to understand each steps of the given algorithm?</p>
+                    <input type="radio" name="q2" value="5"> Yes, I understood each step clearly.<br>
+                    <input type="radio" name="q2" value="4"> I understood the steps, but some parts were unclear.<br>
+                    <input type="radio" name="q2" value="3"> I found it somewhat challenging to understand the steps.<br>
+                    <input type="radio" name="q2" value="2"> No, I had difficulty understanding most of the steps.<br>
+                    <input type="radio" name="q2" value="1"> I did not understand any of the steps.<br>
+                </li><br>
+                <li>
+                    <p>Were you able to solve the excerise provided with the experiment?</p>
+                    <input type="radio" name="q3" value="5"> Yes, was able to solve the excerise without any diffculty.<br>
+                    <input type="radio" name="q3" value="4"> Yes, was able to solve the excerise some diffculty.<br>
+                    <input type="radio" name="q3" value="3"> Yes, was able to solve the excerise  with some aid .<br>
+                    <input type="radio" name="q3" value="2"> No, was not able to solve the excerise.<br>
+                    <input type="radio" name="q3" value="1"> Didn't attempted the excerise.<br>
+                </li><br>
+            </ol>
+    </div><br>
+
+        <label for="feedback">Additional Feedback:</label>
+        <textarea id="feedback" name="feedback" rows="4" maxlength="250"></textarea>
+        <div class = "sub">
+        <input type="submit" value="Submit" id="submit"></div><br>
+        <?php if(isset($_GET['error'])) 
+            {
+           ?> <p id="error"> <?php echo $_GET['error'];} ?></p>
+
+    </form>
+</main>
+
+<script>
+        function validate() {
+            var radios1 = document.getElementsByName("q1");
+            var radios2 = document.getElementsByName("q2");
+            var radios3 = document.getElementsByName("q3");
+            var formValid1 = false;
+            var formValid2 = false;
+            var formValid3 = false;
+
+            for (var i = 0; i < radios1.length; i++) {
+                if (radios1[i].checked) {
+                    formValid1 = true;
+                    break;
+                }
+            }
+
+            for (var i = 0; i < radios2.length; i++) {
+                if (radios2[i].checked) {
+                    formValid2 = true;
+                    break;
+                }
+            }
+
+            for (var i = 0; i < radios3.length; i++) {
+                if (radios3[i].checked) {
+                    formValid3 = true;
+                    break;
+                }
+            }
+
+            if (!(formValid1)) {
+                alert("Please select a option in 1st question!");
+            }
+
+            if (!(formValid2)) {
+                alert("Please select a option in 2nd question!");
+            }
+
+            if (!(formValid3)) {
+                alert("Please select a option in 3rd question!");
+            }
+
+            return formValid1 && formValid2 && formValid3;
+        }
+    </script>
+</body>
+</html>
+
+       
